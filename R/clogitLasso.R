4d8f4af9c9f3fc76682012f15a7f3a919cfb9008
Marta Avalos <marta.avalos-fernandez@u-bordeaux.fr>
2018-06-27 20:34:49 +0000

    version 1.1

--- R/clogitLasso.R
+++ R/clogitLasso.R
@@ -1,7 +1,7 @@
-#'fit lasso for conditional logistic regression for matched case-control studies 
+#'fit lasso for conditional logistic regression for matched case-control studies
 #'
-#' Fit a sequence of conditional logistic regression with lasso penalty,for small to large sized samples
-#' 
+#' Fit a sequence of conditional logistic regression with lasso penalty, for small to large sized samples
+#'
 #' @param X Input matrix, of dimension nobs x nvars; each row is an observation vector
 #' @param y Binary response variable, with 1 for cases and 0 for controls
 #' @param strata Vector with stratum membership of each observation
@@ -16,108 +16,112 @@
 #' @param tol Threshold for convergence-default value is 1e-10
 #' @param epsilon ratio of smallest to largest value of regularisation parameter  at which we find parameter estimates
 #' @param trace If TRUE the algorithm will print out information as iterations  proceed -default is TRUE
-#' @param log If TRUE, fraction are spaced uniformily on the log scale
+#' @param log If TRUE, fraction are spaced uniformly on the log scale
 #' @param adaptive If TRUE adaptive lasso is fitted-default is FALSE
-#' @param separate If TRUE, the weights in adaptive lasso are build serarately using univariate models. Default is FALSE,
+#' @param separate If TRUE, the weights in adaptive lasso are build separately using univariate models. Default is FALSE,
 #' weights are build using multivariate model
 #' @param ols If TRUE, weights less than 1 in adaptive lasso are set to 1. Default is FALSE
 #' @param p.fact Weights for adaptive lasso
 #' @param remove If TRUE, invariable covariates are removed-default is FALSE
 #' @return An object of type \code{clogitLasso} which is a list with the following
 #' components:
-#' 
-#' \item{beta}{nbfraction-by-ncol matrix of estimated coefficients. First row has all 0s}
-#' 
-#' \item{fraction}{A sequence of regularisation parameters at which we obtained the fits}
-#' 
-#' \item{nz}{A vector of length nbfraction containing the number of nonzero parameter estimates for
-#'  the fit at the corresponding regularisation parameter}
-#'   
-#'  \item{arg}{List of arguments}
-#'  @author Marius Kwemou and Marta Avalos
-#'  @details The sequence of models implied by fraction  is fit by  IRLS (iteratively 
-#'  reweighted least squares) algorithm. 
-#'  by coordinate descent with warm starts and sequential strong rules
-#'  @references Avalos, M., Pouyes, H., Grandvalet, Y., Orriols, L., & Lagarde, E. (2015). \emph{Sparse conditional logistic 
-#'  regression for analyzing large-scale matched data from epidemiological studies: a simple algorithm.} BMC bioinformatics, 16(6), 1.
-#'  @importFrom lassoshooting lassoshooting
-#'  @importFrom stats  var aggregate
-#'  @importFrom foreach "%do%" foreach
-#'  @examples
 #'
-#'  #generate data
-#'  y = rep(c(1,0), 100)
-#'  X = matrix (rnorm(20000, 0, 1), ncol = 100) # pure noise
-#'  strata = sort(rep(1:100, 2))
-#'  
-#'  # 1:1
-#'  fitLasso = clogitLasso(X,y,strata,log=TRUE)
-#'  @export
+#' \item{beta}{nbfraction-by-ncol matrix of estimated coefficients. First row has all 0s}
+#'
+#' \item{fraction}{A sequence of regularisation parameters at which we obtained the fits}
+#'
+#' \item{nz}{A vector of length nbfraction containing the number of nonzero parameter estimates for
+#' the fit at the corresponding regularisation parameter}
+#'
+#' \item{arg}{List of arguments}
+#' @author Marta Avalos, Helene Pouyes, Marius Kwemou and Binbin Xu
+#' @details The sequence of models implied by fraction  is fit by  IRLS (iteratively
+#' reweighted least squares) algorithm.
+#' by coordinate descent with warm starts and sequential strong rules
+#' @references Avalos, M., Pouyes, H., Grandvalet, Y., Orriols, L., & Lagarde, E. (2015). \emph{Sparse conditional logistic
+#' regression for analyzing large-scale matched data from epidemiological studies: a simple algorithm.} BMC bioinformatics, 16(6), S1. \doi{10.1186/1471-2105-16-S6-S1}.
+#' @importFrom lassoshooting lassoshooting
+#' @importFrom stats var aggregate
+#' @importFrom foreach "%do%" foreach
+#' @examples
+#' \dontrun{
+#' # generate data
+#' y <- rep(c(1,0), 100)
+#' X <- matrix (rnorm(20000, 0, 1), ncol = 100) # pure noise
+#' strata <- sort(rep(1:100, 2))
+#' 
+#' # 1:1
+#' fitLasso <- clogitLasso(X,y,strata,log=TRUE)
+#' }
+#' @export
 
-clogitLasso = function(X,
+clogitLasso <- function(X,
                        y,
                        strata,
-                       fraction=NULL,
-                       nbfraction=100,
-                       nopenalize=NULL,
-                       BACK=TRUE,
-                       standardize=FALSE,
-                       maxit=100,
-                       maxitB=500,
-                       thr=1e-10,
-                       tol=1e-10,
-                       epsilon=0.0001,
-                       trace=TRUE,
-                       log=TRUE,
-                       adaptive=FALSE,
-                       separate=FALSE,
-                       ols=FALSE,
+                       fraction = NULL,
+                       nbfraction = 100,
+                       nopenalize = NULL,
+                       BACK = TRUE,
+                       standardize = FALSE,
+                       maxit = 100,
+                       maxitB = 500,
+                       thr = 1e-10,
+                       tol = 1e-10,
+                       epsilon = 0.0001,
+                       trace = TRUE,
+                       log = TRUE,
+                       adaptive = FALSE,
+                       separate = FALSE,
+                       ols = FALSE,
                        p.fact = NULL,
-                       remove=FALSE){
-  
-  if(table(strata)[1]==2){
-    res=reg.diff(X=X,
-                 y=y,
-                 strata = strata,
-                 fraction = fraction,
-                 nbfraction = nbfraction,
-                 nopenalize = nopenalize,
-                 BACK =  BACK,
-                 standardize = standardize,
-                 maxit = maxit,
-                 maxitB = maxitB,
-                 thr =thr,
-                 tol = tol,
-                 epsilon =epsilon,
-                 trace = trace,
-                 log = log,
-                 adaptive = adaptive,
-                 separate = separate,
-                 ols = ols,
-                 p.fact = p.fact ,
-                 remove = remove)
-  }else{
-    res = reg.diff1M(X=X,
-                     y=y,
-                     strata=strata,
-                     fraction = fraction,
-                     nbfraction = nbfraction,
-                     nopenalize = nopenalize,
-                     BACK = BACK,
-                     standardize = standardize,
-                     maxit = maxit,
-                     maxitB = maxitB,
-                     thr = thr,
-                     tol = tol,
-                     epsilon = epsilon,
-                     trace = trace,
-                     log =log,
-                     adaptive = adaptive,
-                     separate = separate,
-                     ols = ols,
-                     p.fact = p.fact ,
-                     remove = remove)
+                       remove = FALSE) {
+  if (table(strata)[1] == 2) {
+    res <- reg.diff(
+      X = X,
+      y = y,
+      strata      = strata,
+      fraction    = fraction,
+      nbfraction  = nbfraction,
+      nopenalize  = nopenalize,
+      BACK        =  BACK,
+      standardize = standardize,
+      maxit  = maxit,
+      maxitB = maxitB,
+      thr = thr,
+      tol = tol,
+      epsilon = epsilon,
+      trace   = trace,
+      log     = log,
+      adaptive = adaptive,
+      separate = separate,
+      ols    = ols,
+      p.fact = p.fact,
+      remove = remove
+    )
+  } else{
+    res <- reg.diff1M(
+      X = X,
+      y = y,
+      strata   = strata,
+      fraction = fraction,
+      nbfraction = nbfraction,
+      nopenalize = nopenalize,
+      BACK  = BACK,
+      standardize = standardize,
+      maxit  = maxit,
+      maxitB = maxitB,
+      thr = thr,
+      tol = tol,
+      epsilon = epsilon,
+      trace   = trace,
+      log     = log,
+      adaptive = adaptive,
+      separate = separate,
+      ols    = ols,
+      p.fact = p.fact,
+      remove = remove
+    )
   }
-  class(res) = "clogitLasso"
+  class(res) <- "clogitLasso"
   return(res)
 }
